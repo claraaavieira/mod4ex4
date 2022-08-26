ba055f370906854d31ffb43d6a61f0a3687d11b9
claraaavieira <110980134+claraaavieira@users.noreply.github.com>
2022-08-26 09:37:31 -0300

    Add files via upload

---
+++ import package_banda.banda;.java
@@ -0,0 +1,12 @@
+import package_banda.banda;
+import package_album.album;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      banda a = new banda();
+      album b = new album();
+      a.methodbanda();
+      b.methodalbum();
+
+    }
+}
