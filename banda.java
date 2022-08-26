79a277d4a52388844a1644a4de33e87cea6b530d
claraaavieira <110980134+claraaavieira@users.noreply.github.com>
2022-08-26 09:33:08 -0300

    Add files via upload

---
+++ banda.java
@@ -0,0 +1,9 @@
+//packagebanda.class
+
+package package_banda;
+
+public class banda {
+    public void methodbanda(){
+        System.out.println("Arctic");
+    }
+}
---
+++ package package_album;.java
@@ -0,0 +1,7 @@
+package package_album;
+
+public class album {
+    public void methodalbum(){
+        System.out.println("AM");
+    }
+}
